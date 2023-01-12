993eb45e17b5783b7b949a99435287bb1fffbac4
rebel216 <neerajbhardwajpkt@gmail.com>
2023-01-12 20:31:38 +0530

    add vehical componenets

--- src/components/vehicals/Vehicals.js
+++ src/components/vehicals/Vehicals.js
@@ -1,60 +1,172 @@
-import React ,{useState , useEffect} from 'react'
-import axios from 'axios' 
-import BASE_URL from '../../api';
-import Vehical from '../vehicals/vehical';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
+import DatePicker from 'react-datepicker';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useDispatch } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
+import VehicalCard from './vehical';
 
-const Home = styled.div`
-  text-align:center;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 1200px;
-`
+import newvehical from '../../redux/vehical/vehical.service';
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 20px;
-  width: 100%;
-  padding: 20px;
-  > div {
-    background-color: #fff;
-    border-radius: 5px;
-    padding: 20px;
-  }
-`
+import './vehicallist.scss';
+import BASE_URL from '../../redux/api';
 
-const Vehicals = () => {
-    const [vehicals, setVehicals] = useState([])
-    useEffect(() => {
-        axios.get(`${BASE_URL}api/v1/vehicles/`)
-            .then(resp => {
-                setVehicals(resp.data)
-                
-            
-            })
-        .catch(resp => console.log(resp))
-    }, [vehicals.length])
-    
-    const grid = vehicals.map( (vehical, index) => {
-        const { name, image , details} = vehicals
-    
-        return (
-          <Vehical 
-            key={index}
-            name={name}
-            image_url={image}
-            details={details}
-           
-          />
-        )
-      })
-    
-      return (
-        <Home>
-          
-          <Grid>{grid}</Grid>
-        </Home>
-      )
+const VehicalList = () => {
+  const params = useParams();
+  const [Vehicals, setVehicals] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [message, setMessage] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState(new Date());
+  const [brand, setBrand] = useState('');
+  const [picture, setPicture] = useState('');
+  const [rentalPrice, setRentalPrice] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `${BASE_URL}api/v1/categories/${params.id}`,
+        {
+          headers: {
+            Authorization: `${localStorage.getItem('token')}`,
+          },
+        },
+      );
+      setVehicals(response.data);
+    };
+    fetchData();
+  }, []); 
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (
+      model === ''
+      || year === ''
+      || brand === ''
+      || picture === ''
+      || rentalPrice === ''
+    ) {
+      setMessage('Please fill all the fields');
+    } else {
+      const vehicalData = new FormData();
+      vehicalData.append('Vehical[picture]', picture);
+      vehicalData.append('Vehical[model]', model);
+      vehicalData.append('Vehical[year]', year);
+      vehicalData.append('Vehical[brand]', brand);
+      vehicalData.append('Vehical[rental_price]', rentalPrice);
+      vehicalData.append('Vehical[category_id]', params.id);
+      vehicalData.append('Vehical[reserved]', false);
+      dispatch(newvehical(vehicalData));
+      navigate(`/categories/${params.id}`);
+      setMessage('Vehical created successfully');
     }
-    
-    export default Vehicals
+  };
+
+  return (
+    <div className="wrapper">
+      <div>
+        
+      </div>
+      <div className="vehicallist-header">
+        {message === 'Vehical created successfully' ? (
+          <div className="alert alert-success" role="alert">
+            {message}
+          </div>
+        ) : (
+          <div className="alert alert-danger" role="alert">
+            {message}
+          </div>
+        )}
+      </div>
+      {localStorage.getItem('isAdmin') === 'true' ? (
+        <button type="button" className="addvehicalBtn" onClick={handleShow}>
+          <strong>+</strong>
+          Add Vehical
+        </button>
+      ) : null}
+      {Vehicals.length
+        ? Vehicals.map((Vehical) => (
+          <VehicalCard key={nanoid()} vehical={Vehical} />
+        ))
+        : null}
+
+      <Modal show={show} onHide={handleClose} className="modal">
+        <Modal.Header>
+          <div className="closeModalBtn">
+            <FaTimes onClick={handleClose} />
+          </div>
+          <Modal.Title>
+            <h2>Add New Vehical</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-content">
+          <form className="form-container" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Brand"
+              name="brand"
+              className="form-input"
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Model"
+              name="model"
+              className="form-input"
+              value={model}
+              onChange={(e) => setModel(e.target.value)}
+              required
+            />
+
+            <DatePicker
+              selected={year}
+              onChange={(year) => setYear(year)}
+              name="year"
+              className="form-input"
+              showYearPicker
+              dateFormat="yyyy"
+              required
+            />
+            <input
+              type="number"
+              placeholder="Rental Price"
+              name="rental_price"
+              className="form-input"
+              value={rentalPrice}
+              onChange={(e) => setRentalPrice(e.target.value)}
+              min="0"
+              required
+            />
+            <input
+              type="file"
+              name="image"
+              className="form-input"
+              onChange={(e) => setPicture(e.target.files[0])}
+              required
+            />
+            <Button
+              type="submit"
+              variant="success"
+              onClick={handleClose}
+              className="form-button button"
+            >
+              Add Vehical
+            </Button>
+          </form>
+        </Modal.Body>
+      </Modal>
+
+    </div>
+  );
+};
+
+export default VehicalList;
--- src/components/vehicals/vehical.js
+++ src/components/vehicals/vehical.js
@@ -1,69 +1,98 @@
-import React from 'react'
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { nanoid } from '@reduxjs/toolkit';
+import PropTypes from 'prop-types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import {
+  FaFacebook, FaInstagram, FaTwitter, FaWhatsapp,
+} from 'react-icons/fa';
+import 'swiper/css/bundle';
+import './swiper.css';
+import './vehicalcard.scss';
+import BASE_URL from '../../api';
 
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+const VehicalCard = ({ vehical }) => {
+  const params = useParams();
+  const { Vehicals } = vehical;
 
-const Card = styled.div`
-  border: 1px solid #efefef;
-  background: #fff;
-`
-
-const AirlineLogo = styled.div`
-  height: 50px;
-
-  img {
-    height: 50px;
-    width: 50px;
-    border: 1px solid rgba(0,0,0,0.1);
-    border-radius: 100%;
-  }
-`
-
-const AirlineName = styled.div`
-  padding: 20px 0 10px 0;
-`
-
-const LinkWrapper = styled.div`
-  margin: 30px 0 20px 0;
-  height:50px;
-
-  a {
-    color: #fff;
-    background-color: #71b406;
-    border-radius: 4px;
-    padding: 10px 50px;
-    cursor: pointer;
-    border-radius: 3px;
-    border: 1px solid #71b406;
-    text-align: center;
-    line-height: 20px;
-    min-height: 40px;
-    margin: 7px;
-    font-weight: 600;
-    text-decoration: none;
-    width: 100%;
-    transition: ease-in-out 0.1s;
-
-    &:hover{
-      border-color: #619a07;
-      background: #619a07;
-    }
-  }
-`
-
-const Vehical = () => {
   return (
-    <Card>
-      <AirlineLogo>
-        <img src={prop.image} alt={prop.name} width="50"/>
-      </AirlineLogo>
-      <AirlineName>
-        {prop.name}
-      </AirlineName>
-      <LinkWrapper>
-        <Link to={"/vehicals/" }>View Mobility</Link>
-      </LinkWrapper>
-    </Card>
-  )
-}
+    <>
+      <div className="card-container">
+        <Swiper
+          centeredSlides
+          pagination={{ clickable: true }}
+          spaceBetween={50}
+          slidesPerView="auto"
+          navigation
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {Vehicals.length
+            ? Vehicals.map((el) => (
+              <SwiperSlide key={nanoid()}>
+                <Link to={`/categories/${params.id}/Vehicals/${el.id}`} key={nanoid()}>
 
-export default Vehical
+                  <div className="card card-content">
+                    <div className="img-div">
+                      { el.picture ? (<img src={`${BASE_URL}${el.picture}`} alt="" className="imgSize" />) : (<img src={el.image} alt="" className="imgSize" />)}
+                    </div>
+                    <ul>
+                      <li className="model">
+                        Vehical Model:
+                        {' '}
+                        {el.model}
+                      </li>
+                      <li className="content-id">
+                        ID:
+                        {' '}
+                        {el.id}
+                      </li>
+                    </ul>
+                    <div>
+                      <i className="uil uil-star" />
+                    </div>
+                    <ul className="socials">
+                      <li><FaFacebook /></li>
+                      <li><FaInstagram /></li>
+                      <li><FaTwitter /></li>
+                      <li><FaWhatsapp /></li>
+                    </ul>
+                  </div>
+
+                </Link>
+              </SwiperSlide>
+            ))
+            : null}
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+VehicalCard.defaultProps = {
+  vehical: {
+    catname: '',
+    id: '',
+    Vehicals: [],
+  },
+};
+
+VehicalCard.propTypes = {
+  vehical: PropTypes.shape({
+    catname: PropTypes.string,
+    id: PropTypes.number,
+    Vehicals: PropTypes.arrayOf(
+      PropTypes.shape({
+        brand: PropTypes.string,
+        category_id: PropTypes.number,
+        image: PropTypes.string,
+        model: PropTypes.string,
+        rental_price: PropTypes.number,
+        year: PropTypes.string,
+      }),
+    ),
+  }),
+};
+
+export default VehicalCard;
