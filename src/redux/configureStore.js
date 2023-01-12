85274e868fc88b4e11b299c0f78156fc08461bde
rebel216 <neerajbhardwajpkt@gmail.com>
2023-01-12 20:16:22 +0530

    add reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { delres } from './reservations/delresSlice';
+import delresSlice, { delres } from './reservations/delresSlice';
 import vehicalSlice from './vehical/vehicalSlice';
 import reservationSlice from './reservations/reservationSlice';
+import newvehical from './vehical/newVehical';
+import delvehicalSlice from './vehical/delvehicalSlice';
 
 const store = configureStore({
     reducer: {
+        addvehical:newvehical,
         vehical: vehicalSlice,
+        delvehical:delvehicalSlice,
         reservation: reservationSlice,
-        myreservation: delres
+        delreservation: delresSlice,
     }
 });
 
---
+++ src/redux/vehical/newVehical.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import BASE_URL from '../../api"';
+
+const newvehical = createAsyncThunk(
+  'createvehical/createvehical',
+  async (resource) => {
+    const response = await axios.post(`${BASE_URL}api/v1/add`, resource, {
+      headers: {
+        Authorization: `${localStorage.getItem('token')}`,
+      },
+    });
+    const res = await response.data;
+    return res;
+  },
+);
+
+export default newvehical;
