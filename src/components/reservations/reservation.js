28d8895b3ef35c0261d98173f075e896bc01acf5
Aleazar yilma <yilma4060@gmail.com>
2023-01-15 18:13:20 +0300

    update vehical details style

--- src/components/reservations/reservation.js
+++ src/components/reservations/reservation.js
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
-const addReservation = () => {
-  
+const AddReservation = () => {
+
 
   return (
-    <div className="wrapper">
-      
-      <div className="reservation-container">
-        <h1>Make a Reservation</h1>
-        </div>
+    <div className="container ">
+      <span className='add-vehicle-header'> Reserve</span>
+      <div className="row">
+        <form className='add-vehicle-form'>
+          <div className="form-group">
+            <label>email</label>
+            <input type="text" className="form-control" id="formGroupExampleInput" disabled />
+          </div>
+          <div className="form-group">
+            <label>Date</label>
+            <input type="date" className="form-control" id="formGroupExampleInput2" required />
+          </div>
+          <div className="form-group">
+            <label>Address</label>
+            <input type="text" className="form-control" id="formGroupExampleInput2" required />
+          </div>
+          <div className="form-group">
+            <label >Vehicle</label>
+            <select className="form-control" id="exampleFormControlSelect1">
+              <option> car 1</option>
+              <option> car 2</option>
+              <option> car 3</option>
+              <option> car 4</option>
+              <option> car 5</option>
+            </select>
+          </div>
+
+          <div className='button-green'>
+            <button type="button" className="btn btn-primary btn-lg btn-block detail-reserve-btn">Reserve</button>
+
+          </div>
+        </form>
+
+      </div>
     </div>
   );
 };
 
-export default addReservation;
+export default AddReservation;
